f2b9673bb73dcc426c6ca09251c0a78148a5c3ec
Dillon Draper <dillonbdraper@gmail.com>
2021-02-18 14:57:46 -0600

    Made initial migrations translating ERD to database

--- rareapi/models/subscriptions.py
+++ rareapi/models/subscriptions.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Subscription(models.Model):
 
-    follower = models.ForeignKey("RareUser", on_delete=models.CASCADE)
+    follower = models.ForeignKey("RareUser", on_delete=models.CASCADE, related_name="poster")
     author = models.ForeignKey("RareUser", on_delete=models.CASCADE)
     created_on = models.DateTimeField(auto_now=False, auto_now_add=False)
     ended_on = models.DateTimeField(auto_now=False, auto_now_add=False)
