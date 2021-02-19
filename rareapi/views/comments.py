7a9e4b52c9c062a177a35dc3c891ee86201e0658
Dillon Draper <dillonbdraper@gmail.com>
2021-02-19 13:15:57 -0600

    Added retrieve/destroy/update methods for comments and tested in postman

--- rareapi/views/comments.py
+++ rareapi/views/comments.py
@@ -34,6 +34,40 @@ class Comments(ViewSet):
         except ValidationError as ex:
             return Response({"reason": ex.message}, status=status.HTTP_400_BAD_REQUEST)
 
+    def retrieve(self, request, pk=None):
+
+        try:
+            comment = Comment.objects.get(pk=pk)
+            serializer = CommentSerializer(comment, context={'request': request})
+            return Response(serializer.data)
+        except Exception as ex:
+            return HttpResponseServerError(ex)
+
+    def update(self, request, pk=None):
+
+        comment = Comment.objects.get(pk=pk)
+        comment.content = request.data['content']
+        comment.created_on = date.today()
+
+        comment.save()
+
+        return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+
+    def destroy(self, request, pk=None):
+
+        try:
+            comment = Comment.objects.get(pk=pk)
+            comment.delete()
+
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+        except Comment.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+        except Exception as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
 
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
