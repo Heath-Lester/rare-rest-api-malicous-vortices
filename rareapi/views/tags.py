5128ac27d80034a3ab9250b1f8ac9dac84620807
Dillon Draper <dillonbdraper@gmail.com>
2021-02-19 11:50:39 -0600

    Added delete functionality for tags.  Super simple/small pull request

--- rareapi/views/tags.py
+++ rareapi/views/tags.py
@@ -34,6 +34,20 @@ class Tags(ViewSet):
         tag.save()
         return Response({}, status=status.HTTP_204_NO_CONTENT)
 
+    def destroy(self, request, pk=None):
+
+        try:
+            tag = Tag.objects.get(pk=pk)
+            tag.delete()
+
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+        except Tag.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+        except Exception as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
 
 class TagSerializer(serializers.ModelSerializer):
 
