06e9e1551643e54b730b88145ae9b436564f188d
billwishop <willbishop.hello@gmail.com>
2021-02-24 09:52:31 -0600

    The comments model now includes a custom property that indicates whether the logged in user is the author of the comment.

--- rareapi/models/comments.py
+++ rareapi/models/comments.py
@@ -6,3 +6,11 @@ class Comment(models.Model):
     author = models.ForeignKey("RareUser", on_delete=models.CASCADE)
     content = models.CharField(max_length=2000)
     created_on = models.DateField()
+
+    @property
+    def my_comment(self):
+        return self.__my_comment
+
+    @my_comment.setter
+    def my_comment(self, value):
+        self.__my_comment = value
